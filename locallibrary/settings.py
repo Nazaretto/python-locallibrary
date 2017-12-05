77d0aad3de00a78c847b090d5c57382b5163c6b1
Nazar Trukhan <nazar0truhan@gmail.com>
2017-12-05 15:17:32 +0200

    Update ALLOWED_HOSTS with site and development server URL

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '#lu1!z2v&%opayne2w3xf%w)f=21cq
 #for production
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['locallobrary.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
